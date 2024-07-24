90d4d0297bf67fe8ac77ac6fe4721856eb959e67
Lucas <lucasdasilva9234@gmail.com>
2024-07-23 22:32:29 -0300

    Inicio

---
+++ santander-java-bootcamp/src/main/java/me/dio/santander_java_bootcamp/SantanderJavaBootcampApplication.java
@@ -0,0 +1,14 @@
+package me.dio.santander_java_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderJavaBootcampApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SantanderJavaBootcampApplication.class, args);
+	}
+
+}
